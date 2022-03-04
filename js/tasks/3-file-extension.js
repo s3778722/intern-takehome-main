2fa880f4e0d38d1c1b03a52b70edceade7165104
Han Chien <hanchienleow1@gmail.com>
2022-03-04 23:35:24 +1100

    Solution for task3-file-extension

--- js/tasks/3-file-extension.js
+++ js/tasks/3-file-extension.js
@@ -6,5 +6,12 @@
  * @returns {String} The file extension (with no period), or false if there is none.
  */
 export default function getFileExtension(filename) {
-
+    //Split the string into an array of substrings between '.' .
+    //Pop to get the last substring.
+    const fileExtension = filename.split('.').pop()
+    //If there's no file extension, return false
+    if (fileExtension === filename){
+        return false
+    }
+    return fileExtension
 }
