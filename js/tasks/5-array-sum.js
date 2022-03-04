b25b32ff82525b00b5f9629faaf387b26576f75a
Han Chien <hanchienleow1@gmail.com>
2022-03-05 00:04:43 +1100

    Solution for task 5-array-sum

--- js/tasks/5-array-sum.js
+++ js/tasks/5-array-sum.js
@@ -5,6 +5,25 @@
  * @param {Array} startOfTree An array containing other arrays, ints, strings..
  * @returns {Number} The sum of all integers contained in the input, at any level.
  */
-export default function arraySum(startOfTree) {
 
+/** 
+ *  Time Complexity: O(n), n being the length of the elements in the array
+ *  Space Complexity: O(d), d being the depth of the array 
+*/
+export default function arraySum(startOfTree) {
+    //declare the result variable as sum
+    let sum = 0
+    //Loop through the tree
+    startOfTree.forEach(element => {
+        //Check if the type of the element is number
+        if (typeof element === 'number'){
+            //Add it to the sum
+            sum += element
+        //Else if the element is an array
+        } else if ( element instanceof Array){
+            //Perform arraySum recursively.
+            sum += arraySum(element)
+        }
+    });
+    return sum
 }
